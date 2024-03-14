8f63a0f9feb5ca0ec6b73a0c427be85198d5b941
Priyo0011 <priyotosh0011@gmail.com>
2024-03-14 23:21:50 +0600

    added blogs section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
+
 
 function App() {
 
 
   return (
     <>
-
-      <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
-
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
